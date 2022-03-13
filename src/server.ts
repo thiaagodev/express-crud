4eeb2890c5413ca026221dd4a6b46d66e93ac988
Thiago Ferreira <thiaago.dev@gmail.com>
2022-03-13 18:16:15 -0300

    Swagger

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
 import express from 'express'
 import router from "./routes"
+import * as swaggerUi  from 'swagger-ui-express'
+
+const swaggerDocument = require('./swagger_output.json')
 
 const app = express()
 
@@ -7,4 +10,5 @@ app.use(express.json())
 app.use(router)
 
 app.listen(3000, () => console.log('Server is running at http://localhost:3000'))
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
